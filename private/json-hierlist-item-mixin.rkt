0d09ea1514decb19eb3c882b288d65423a09e95a
Philippe Mechai <philippe.mechai@gmail.com>
2018-08-27 22:06:12 +0200

    code cleanup

--- private/json-hierlist-item-mixin.rkt
+++ private/json-hierlist-item-mixin.rkt
@@ -3,22 +3,21 @@
 (require mrlib/hierlist)
 
 (provide json-hierlist-item-mixin)
-  
+
+(define color-key-prefix "plt:framework-pref:color-scheme-entry:framework:syntax-color:scheme:")
+(define font-name-key 'plt:framework-pref:framework:standard-style-list:font-name)
+(define font-size-key 'plt:framework-pref:framework:standard-style-list:font-size)
+
 (define (get-color-from-preference name)
-  (define key (string->symbol
-               (string-append "plt:framework-pref:color-scheme-entry:framework:syntax-color:scheme:"
-                              (symbol->string name))))
-  (define pref (get-preference key))
-  (apply make-object (cons color%
-                           (seventh (hash-ref pref 'classic)))))
+  (define pref (get-preference (string->symbol (~a color-key-prefix name))))
+  (apply make-object (cons color% (seventh (hash-ref pref 'classic)))))
 
 (define (make-style-delta style)
   (define delta (new style-delta%))
   (send* delta
     (set-delta-foreground (get-color-from-preference style))
-    (set-face (get-preference 'plt:framework-pref:framework:standard-style-list:font-name))
-    (set-size-add (vector-ref
-                   (get-preference 'plt:framework-pref:framework:standard-style-list:font-size) 1))
+    (set-face (get-preference font-name-key))
+    (set-size-add (vector-ref (get-preference font-size-key) 1))
     (set-size-mult 0))
   delta)
 
@@ -45,14 +44,12 @@
         (else key-delta)))
 
     (define/public (insert-styled-text style str)
-      (define ed (get-editor))
-      (send* ed
+      (send* (get-editor)
         (change-style (get-style-delta style))
         (insert str)))
 
     (define/public (insert-value value)
-      (define t (get-editor))
-      (send* t
+      (send* (get-editor)
         (change-style (get-value-style-delta value))
         (insert (~v value))))
 
