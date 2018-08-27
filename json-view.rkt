0e1090d9b1ceb59aa2e3dcff92088781c362a1b4
Philippe Mechai <philippe.mechai@gmail.com>
2018-08-27 22:07:39 +0200

    remove forgotten commented code

--- json-view.rkt
+++ json-view.rkt
@@ -2,24 +2,3 @@
 
 (require "private/json-view.rkt")
 (provide (all-from-out "private/json-view.rkt"))
-
-#|
-(require json
-         mrlib/hierlist
-         "private/json-view.rkt")
-
-(define (load-json filename)
-  (with-input-from-file filename
-    (lambda () (read-json))))
-
-(define j (load-json
-           ;"colors.json"
-           "/Users/mehdi/Downloads/reddit-racket.json"
-           ;"/Users/mehdi/src/ATTIC/scm/apricot/db2.json"
-           ))
-
-(define f (new frame% [label "JSON Viewer"] [width 600] [height 800]))
-(define hl (new json-view% [parent f]))
-(send hl set-json! j)
-(send f show #t)
-|#
