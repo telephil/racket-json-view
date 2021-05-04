168680e131b8c75c686c507d52fca2c84f972074
Tom Gillespie <tgbugs@gmail.com>
2021-05-04 13:02:30 -0700

    initial implementation of partial expansion of json -> hierlist
    When loading large json files building the hierlist takes a extremely
    long time, locking the interface. This commmit modifies how the hlist
    is built so that nested json objects are only loaded as hierlists when
    they are opened. This significantly improves performance.
    
    One optimization that has not beend implemented is to remove rendered
    objects from the unexpanded cache once they have been rendered.

--- private/json-hierlist.rkt
+++ private/json-hierlist.rkt
@@ -10,6 +10,7 @@
   (class hierarchical-list%
     (init-field [on-item-select values])
     (define node-cache (make-hash))
+    (define unexpanded (make-hash))
     (field [root #f])
     
     (define/private (new-item-node parent)
@@ -33,7 +34,7 @@
         ((list? value) 'list)
         (else 'value)))
 
-    (define/private (create-key-value-tree parent parent-path key value kind style)
+    (define/private (create-key-value-tree parent parent-path key value kind style [once 0])
       (if (atom? value)
           (let ((node (new-item-node parent))
                 (path (reverse (cons key parent-path))))
@@ -45,16 +46,18 @@
             (hash-set! node-cache path node))
           (let ((node (new-list-node parent parent-path key kind style))
                 (path (cons key parent-path)))
-            (create-tree value node path))))
+            (if (< once 1)
+                (hash-set! unexpanded path value)
+                (create-tree value node path (sub1 once))))))
 
-    (define/private (create-tree jsexpr parent path)
+    (define/private (create-tree jsexpr parent path [once 1])
       (cond
         ((hash? jsexpr)
          (for (((key value) (in-hash jsexpr)))
-              (create-key-value-tree parent path key value (get-value-type value) 'key)))
+              (create-key-value-tree parent path key value (get-value-type value) 'key once)))
         ((list? jsexpr)
          (for (((value index) (in-indexed jsexpr)))
-              (create-key-value-tree parent path index value (get-value-type value) 'index)))
+              (create-key-value-tree parent path index value (get-value-type value) 'index once)))
         (else
          (let ((node (new-item-node parent))
                (path (reverse (cons jsexpr path))))
@@ -62,6 +65,16 @@
              (insert-value jsexpr)
              (user-data (node-data 'value jsexpr jsexpr path)))
            (hash-set! node-cache path node)))))
+
+    (define/override (on-item-opened item)
+      (let ([items (send item get-items)])
+        (when (and (memq (node-data-type (send item user-data)) '(hash list))
+                   (null? items))
+          (let* ([nd (send item user-data)]
+                 [ndp (reverse (node-data-path nd))]
+                 [hr (hash-ref unexpanded ndp #f)])
+            (when hr
+              (create-tree hr item ndp))))))
 
     (define/override (on-select item)
       (when item
