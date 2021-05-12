dc8714df8ca183e0141809985aba1ba6e12752a4
Tom Gillespie <tgbugs@gmail.com>
2021-05-11 20:52:13 -0700

    json-view% make it possible to pass the hierlist during init
    also provied json-hierlist% so that it can be subclassed to customize

--- private/json-view.rkt
+++ private/json-view.rkt
@@ -5,18 +5,20 @@
          "node-data.rkt")
 
 (provide json-view%
+         json-hierlist%
          (struct-out node-data))
 
 (define json-view%
   (class vertical-panel%
     (super-new)
+    (init-field [hier-class% json-hierlist%])
 
     (field [path-bar (new breadcrumb%
                           [parent this]
                           [callback (lambda (path)
                                       (send json-hierlist select-path path))])])
 
-    (field [json-hierlist (new json-hierlist%
+    (field [json-hierlist (new hier-class%
                                [parent this]
                                [on-item-select (lambda (data)
                                                  (send path-bar set-path!
